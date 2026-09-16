23e65f1fbc0e49519cfe477df37c87072ef9288a
Mohit Sahoo <iamblackwizard2@gmail.com>
2026-09-16 08:07:50 +0000

    Add 'Add, Remove and Clear Items' functionality

---
+++ script.js
@@ -0,0 +1,58 @@
+const itemForm = document.querySelector("#item-form");
+const itemInput = document.querySelector("#item-input");
+const itemList = document.querySelector("#item-list");
+const clearBtn = document.querySelector("#clear");
+
+// Functions
+function addItem(e) {
+  e.preventDefault();
+  const newItem = itemInput.value;
+
+  // Validate Input
+  if (newItem === "") {
+    alert("Please add something!");
+    return;
+  }
+
+  //   Creating List Items
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+
+  itemInput.value = "";
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems(e) {
+  while (itemList.firstChild){
+    itemList.removeChild(itemList.firstChild)
+  }
+}
+
+// Event Listeners
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
